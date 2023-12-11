39e8b942781a9ec886f1351669f6b4a2f3aa85d7
Zagidin <prap0235@gmail.com>
2023-12-12 02:59:29 +0300

    Zagidin

---
+++ PZ_5/func.py
@@ -0,0 +1,21 @@
+"""
+Описать функцию Powerl(A, B) вещественного типа, находящую величину AB по
+формуле AB=exp(B*ln(A)) (параметры A и B -- вещественные). В случае нулевого
+или отрицательного параметра A фунция возращает 0. С помощью этой функции
+найти степени A^P, B^P, C^P, если даны числа P, A, B, C
+
+"""
+from math import exp, log
+
+
+def powerl(a, b):
+
+    if a <= 0:
+        return 0
+    else:
+        return exp(b * log(a))
+
+
+num1, num2, num3 = float(input('Введите число A: ')), float(input('Введите число B: ')), int(input('Введите число С: '))
+stepen = int(input('Введите степень: '))
+print(f'Результат первого условия: {powerl(num1, num2)}\n\nРезультат второго условия:\n\t{powerl(num1, stepen)}\n\t{powerl(num2, stepen)}\n\t{powerl(num3, stepen)}')
--- PZ_5/summa.py
+++ PZ_5/summa.py
@@ -1,4 +1,5 @@
-def summ_namber(number1, number2):
+# Создать функцию, которая выполнит суммирование числового ряда.
+def summ_nambers(number1, number2):
     contenner = 0
     # цикл от числа1 до числа2
     for i in range(number1, number2+1):
@@ -9,4 +10,4 @@ def summ_namber(number1, number2):
 number1 = int(input('Введите начало числового ряда: '))
 number2 = int(input('Введите конец числового ряда: '))
 
-print(f'Результат суммирования числового ряда: {summ_namber(number1, number2)}')
+print(f'Результат суммирования числового ряда: {summ_nambers(number1, number2)}')
