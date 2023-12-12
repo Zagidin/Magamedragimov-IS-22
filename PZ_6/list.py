86485c66bdb29adc08fe0559152f9db537c1c40c
Zagidin <prap0235@gmail.com>
2023-12-13 01:27:55 +0300

    Zagidinchik

--- PZ_6/list.py
+++ PZ_6/list.py
@@ -8,11 +8,11 @@ if 1 < k < l < len_list:
     for i in range(1, len_list+1):
         my_list.append(i)
 
-        if i == k or i == l:
+        if k <= i <= l:
             continue
 
         list_not_KL.append(i)
 
-    print(f'Ваш созданный список с целыми числами K({k}), L({l}) заданной длиной N({len_list}): {my_list}\nСумма вашего списка не учитывая числа K и L: {list_not_KL}')
+    print(f'Ваш созданный список с целыми числами K({k}), L({l}) заданной длиной N({len_list}): {my_list}\nСумма вашего списка не учитывая числа K и L: {sum(list_not_KL)}')
 else:
     print('Вы ввели что-то не по условию, удостоверьтесь что 1<K<L<N или число длины целое положительное!)')
