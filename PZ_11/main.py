d7d7a8fecd8bddfc997481b34a2ca72ceae43896
Zagidin <prap0235@gmail.com>
2024-01-28 22:09:56 +0300

    Zagidin

--- PZ_11/main.py
+++ PZ_11/main.py
@@ -14,23 +14,40 @@
 Сумма положительных элементов.
 """
 
-negative_elements = [-1, -2, -3, -4, -5]
-
-with open('./PZ_11/sample.txt', 'w') as file_1:
-    file_1.write(str(negative_elements))
-    
-with open('./PZ_11/sample.txt', 'r') as file_1:
-    len_content_number = file_1.readline()
-    
-len_content_number = len_content_number.split()
-len_content_number = len_content_number.replace('[', '')
-
-for i in range(len(len_content_number)):
-    len_content_number[i] = int(len_content_number[i])
-
-# quantity_negative = 0
-# for i in range(len(len_content_number)):
-#     if len_content_number[i] < 0:
-#         quantity_negative += 1
-
-print(len_content_number)
+from random import randint 
+ 
+numbers = [] 
+ 
+for i in range(10): 
+    i = randint(-10, 10) 
+    numbers.append(i) 
+ 
+with open('PZ_11/fail_1.txt', 'w') as fail_1: 
+     
+    num_negative = [] 
+ 
+    for el in numbers: 
+        if el < 0: 
+            num_negative.append(el) 
+        else: 
+            continue 
+ 
+    quantity_negative_digit = len(num_negative) 
+     
+    fail_1.write(f'Negative elements: {str(num_negative)}\nThe number of negative numbers: {quantity_negative_digit}\nthe average value of the numbers: {sum(num_negative) / quantity_negative_digit}') 
+ 
+with open('PZ_11/fail_2.txt', 'w') as fail_2: 
+ 
+    num_positive = [] 
+ 
+    for j in numbers: 
+        if j > 0: 
+            num_positive.append(j) 
+        else: 
+            continue 
+ 
+    quantity_positive_digit = len(num_positive) 
+     
+    fail_2.write(f'Positive elements: {str(num_positive)}\nThe number of positive numbers: {quantity_positive_digit}\nThe sum of positive numbers: {sum(num_positive)}') 
+ 
+print(f"\nВаш список: {numbers}\n")
