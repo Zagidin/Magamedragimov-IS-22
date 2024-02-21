77e92f917cb558144c4696d76529bf4beac6724d
Zagidin <prap0235@gmail.com>
2024-02-21 11:05:57 +0300

    Zagidin

--- PZ_11/task2.py
+++ PZ_11/task2.py
@@ -8,12 +8,12 @@ with open('PZ_11/text18-21.txt', 'r', encoding='UTF-16') as file:
     content = file.read()
     print(content)
     
-# Считаем количество знаков препинания
-    punctuation_count = sum([1 for char in content if char in '.,:;!?'])
-    print(f'Количество знаков препинания: {punctuation_count}')
+    punctuation = sum([1 for char in content if char in '.,:;!?'])
+    print(f'Количество знаков препинания: {punctuation}')
 
-# Формируем новый файл с текстом в стихотворной форме
 lines = content.split('\n')
+
 reversed_lines = '\n'.join(lines[::-1])
 with open('PZ_11/Стих.txt', 'w', encoding='utf-8') as new_file:
     new_file.write(reversed_lines)
+    
